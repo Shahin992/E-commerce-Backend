3c3a1ff834bbbfa387689732d82923a826079128
Shahin khan <Shahin992>
2024-05-21 18:38:54 +0600

    update product

--- src/modules/products/product.controller.ts
+++ src/modules/products/product.controller.ts
@@ -2,13 +2,14 @@ import { Request, Response, NextFunction } from 'express';
 import {productValidationSchema} from '../../../Utils/ErrorValidation'
 import productModel from './product.model';
 import { CLIENT_RENEG_LIMIT } from 'tls';
+
+
 export const createProduct = async (req: Request, res: Response, next: NextFunction) => {
   const validationResult = productValidationSchema.safeParse(req.body);
-
-  if (!validationResult.success) {
-    const error = validationResult.error;
-    return res.status(400).json({ success: false, message: error.errors[0].message });
-  }
+    if (!validationResult.success) {
+      const error = validationResult.error;
+      return res.status(400).json({ success: false, message: error.errors[0].message });
+    }
 
   try {
     const product = new productModel(req.body);
@@ -33,6 +34,16 @@ export const getProductById = async (req: Request, res: Response, next: NextFunc
     const product = await productModel.findById(req.params.productId);
     if (!product) return res.status(404).json({ success: false, message: 'Product not found' });
     res.status(200).json({ success: true, message: 'Product fetched successfully!', data: product });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateProduct = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const product = await productModel.findByIdAndUpdate(req.params.productId, req.body, { new: true });
+    if (!product) return res.status(404).json({ success: false, message: 'Product not found' });
+    res.status(200).json({ success: true, message: 'Product updated successfully!', data: product });
   } catch (err) {
     next(err);
   }
--- src/modules/products/product.route.ts
+++ src/modules/products/product.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createProduct, getProductById, getProducts } from './product.controller';
+import { createProduct, getProductById, getProducts, updateProduct } from './product.controller';
 
 const router = Router();
 
 router.post('/', createProduct);
 router.get('/', getProducts);
 router.get('/:productId', getProductById);
+router.put('/:productId', updateProduct);
 
 export default router;
